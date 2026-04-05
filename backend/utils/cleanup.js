0c1fa049cdc91d5ad108a6d95acf53058bb18477
Nagendar <varkalanagendar777@gmail.com>
2026-04-05 22:49:10 +0530

    hide the sensitive files

---
+++ backend/utils/cleanup.js
@@ -0,0 +1,11 @@
+import cron from 'node-cron'
+import fs from 'fs';
+import path from 'path'
+
+const cleanExpiredFiles = async(req,res)=>{
+    try{
+        const currentTime = new Date()
+        const timeString = now.toLocaleTimeString();
+        console.log(timeString)
+    }
+}
